61ea5d0b5c4b175a889c8160c5d571b7ac02f2fd
Libby Eilbert <elizabeth.eilbert@gmail.com>
2024-01-14 20:18:21 -0800

    initialized spring application

---
+++ src/main/java/com/example/brittletracker/BrittletrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.brittletracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrittletrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrittletrackerApplication.class, args);
+	}
+
+}
